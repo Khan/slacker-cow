a63ade8b72957fbaa1c74be1afe9a637b8c9d36b
Matthew Rothenberg <mroth@khanacademy.org>
2015-09-02 18:02:47 -0400

    safer regex match
    Summary:
    This won’t match Phabricator IDs that are already in URLs, and also
    matches T[ask] IDs.
    
    Test Plan: verified in local docker shell
    
    Reviewers: benjaminpollack
    
    Reviewed By: benjaminpollack
    
    Differential Revision: https://phabricator.khanacademy.org/D21304

--- hubot/scripts/phabrifurl.js
+++ hubot/scripts/phabrifurl.js
@@ -1,10 +1,11 @@
 // All this does is linkify Phabricator items.
 export default robot => {
-  robot.hear(/\b(D\d+)\b/, msg => {
-    if (msg.match[1] === 'D20') {
+  robot.hear(/(?:^|\s)([DT]\d{2,})\b/, msg => {
+    const phabID = msg.match[1];
+    if (phabID === 'D20') {
       msg.send('Impressive roll, Padawan Sulu. Oh my.');
     } else {
-      msg.send(`https://phabricator.khanacademy.org/${msg.match[1]}`);
+      msg.send(`:phabricator: <https://phabricator.khanacademy.org/${phabID}|${phabID}>`);
     }
   });
 };
